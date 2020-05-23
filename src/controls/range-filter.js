f73bfde3eb599e0056b3417fffec0775877b3f1f
NithinSastry <nithin_Sastry@outlook.com>
2020-05-23 23:02:57 +0530

    Range filter markup.

--- src/controls/range-filter.js
+++ src/controls/range-filter.js
@@ -1,6 +1,32 @@
 class RangeFilter {
-    constructor() {
-        this.minValue = 0;
-        this.maxValue = 
+  constructor() {
+    this.minValue = 0;
+    this.maxValue = 1000;
+  }
+
+  getOptions = () => {
+    let options = ``;
+    for (let i = 0; i <= 1000; i += 100) {
+      options += `
+            <option value=${i}>${i}</option>
+        `;
     }
+    return options;
+  };
+
+  getMarkup = () => {
+    return `
+        <div class="range-filter">
+            <select>
+                ${this.getOptions()}
+                </select>
+                <p>to</p>
+                <select>
+                ${this.getOptions()}
+                </select>
+        </div>
+      `;
+  };
 }
+
+export default RangeFilter;
--- src/views/filter-view.js
+++ src/views/filter-view.js
@@ -1,11 +1,18 @@
+import RangeFilter from '../controls/range-filter';
 class FilterView {
-    getMarkup = () => {
-        return `
+  constructor() {
+    this.rangeFilter = new RangeFilter();
+  }
+  getMarkup = () => {
+    return `
             <div id="filter">
                 <p>Filter markup</p>
+                <div>
+                    ${this.rangeFilter.getMarkup()}
+                </div>
             </div>
-        `
-    }
+        `;
+  };
 }
 
 export default FilterView;
